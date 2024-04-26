993113a8477f45292e3172102943be958d712b9f
Simone Tolomei <tolomei.simone5@gmail.com>
2024-04-26 12:13:33 +0200

    Update build.sh to set CMAKE_BUILD_TYPE to Release

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -e
 # Set the default build type
 BUILD_TYPE=RelWithDebInfo
 source /opt/ros/humble/setup.bash
-colcon build  --symlink-install 
+colcon build  --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
